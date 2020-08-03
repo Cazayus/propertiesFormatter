982922c9d28a9d74cec096bd6ef7cf8cb25c8ecd
GitHub Action <action@github.com>
2020-08-03 08:19:07 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/cazayus/propertiesformatter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.cazayus.propertiesformatter.services
+
+import com.github.cazayus.propertiesformatter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
